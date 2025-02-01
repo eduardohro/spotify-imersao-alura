04e6f71a7cd0475be179d22aeca3de29e26c2b17
Eduardo Rodrigues Oliveira <159492869+eduardohro@users.noreply.github.com>
2025-02-01 20:28:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,32 +3,40 @@ const resultArtist = document.getElementById("result-artist");
 const resultPlaylist = document.getElementById('result-playlists');
 
 function requestApi(searchTerm) {
-    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`;
     fetch(url)
         .then((response) => response.json())
-        .then((result) => displayResults(result))
+        .then((result) => displayResults(result));
 }
 
 function displayResults(result) {
-    resultPlaylist.classList.add("hidden")
-    const artistName = document.getElementById('artist-name');
-    const artistImage = document.getElementById('artist-img');
+    resultPlaylist.classList.add("hidden");
+    resultArtist.innerHTML = '';
 
     result.forEach(element => {
+        const artistDiv = document.createElement('div'); 
+        const artistName = document.createElement('h3'); 
+        const artistImage = document.createElement('img'); 
+
         artistName.innerText = element.name;
         artistImage.src = element.urlImg;
+
+        artistDiv.appendChild(artistImage); 
+        artistDiv.appendChild(artistName); 
+        resultArtist.appendChild(artistDiv); 
     });
 
-    resultArtist.classList.remove('hidden');
+    resultArtist.classList.remove('hidden'); 
 }
 
 document.addEventListener('input', function () {
     const searchTerm = searchInput.value.toLowerCase();
     if (searchTerm === '') {
         resultPlaylist.classList.add('hidden');
-        resultArtist.classList.remove('hidden');
-        return
+        resultArtist.classList.add('hidden'); 
+        return;
     }
     
     requestApi(searchTerm);
-})
+});
+
