b8796ade09a6f26c442cce4d384f21a74aa9d899
Eduardo Rodrigues Oliveira <159492869+eduardohro@users.noreply.github.com>
2025-02-01 18:47:17 -0300

    add script api

---
+++ script.js
@@ -0,0 +1,23 @@
+const searchInput = document.getElementById('search-input');
+const resultsArtist = document.getElementById('result-artist');
+const resultPlaylist = document.getElementById('result-playlists');
+
+function requestApi(searchTerm) {
+    const url = 'http://localhost:3000/artists?name_like=${searchTerm}'
+    fetch(url)
+        .then((response) => response.json())
+        // .then((result) => displayResults(result))
+}
+
+
+
+document.addEventListener('input', function() {
+    const searchTerm = searchInput.ariaValueMax.toLowerCase();
+    if (searchTerm === '') {
+        resultPlaylist.classList.add('hidden');
+        resultsArtist.classList.remove('hidden');
+        return;
+    }
+
+    requestApi(searchTerm);
+})
